9b975da13dc3cac358e76dbd04ce1fd5dfa5882e
vguenoff <vguenoff@gmail.com>
2022-04-16 14:26:35 +0300

    Simplify

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
-                    className={styles.link}
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
